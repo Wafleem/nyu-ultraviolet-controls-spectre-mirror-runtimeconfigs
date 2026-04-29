3f7e6caae64663ce6b7a62806e791c8ba8864949
Ramon Li <rl4694@nyu.edu>
2026-04-29 11:47:32 -0400

    Tune up yaw lead

--- application/gimbal/gimbal_controller.h
+++ application/gimbal/gimbal_controller.h
@@ -14,7 +14,7 @@ extern "C" {
 // #define MAX_YAW_ANGLE_PER_SEC   2000.0f
 #define MAX_YAW_ANGLE           360.0f
 #define MAX_YAW_ANGLE_CHANGE    150.0f
-#define MAX_YAW_TARGET_LEAD       20.0f  // deg: how far angle_target can lead actual yaw (tune up if sluggish)
+#define MAX_YAW_TARGET_LEAD       50.0f  // deg: how far angle_target can lead actual yaw (tune up if sluggish)
 #define MAX_PITCH_ANGLE         8192.0f   // Can be overwritten by robot config
 #define MAX_PITCH_ANGLE_CHANGE  1500.0f
 
